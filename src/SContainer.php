d9e1bc26aa39aa49c77481116f8c31a494ce77b0
Gabi <Gabi@Gabi-PC>
2016-04-01 19:55:15 +0300

    container works now

--- src/SContainer.php
+++ src/SContainer.php
@@ -9,58 +9,26 @@ use GabiDJ\Expressive\SContainer\Exception\ServiceNotFoundException;
 class SContainer implements ContainerInterface
 {
 	/**
-	 * 
+	 *
 	 * @var unknown $_services
 	 */
 	private $_services = array();
 	
-	/**
-	 * 
-	 * @var unknown
-	 */
 	private $_factories = array();
 	
-	/**
-	 * 
-	 * @var unknown
-	 */
 	private $_invokables = array();
 	
-	/**
-	 * Injectable constructor.
-	 * @access public
-	 * @param $config 
-	 * @param SContainer|null $router
-	 */
-	public function __construct($configuration = array(), SContainer $container = null)
+	public function getMethodParams($class, $method ='__invoke')
 	{
-		if($container instanceof ContainerInterface)
+		$reflectionClass = new \ReflectionClass($class);
+		if(!$reflectionClass->hasMethod($method))
 		{
-			return $container;
+			return false;
 		}
-		
-		
-		foreach($configuration['dependencies']['invokables'] as $interface => $implementation)
-		{
-			$this->_services[$interface] = new ServiceWrapper(new $implementation());
-		}
-		
-		foreach($configuration['dependencies']['factories'] as $className => $factoryName)
-		{
-			$this->_services[$factoryName] = new $factoryName();
-			$this->_services[$className] = new ServiceWrapper($this->_services[$factoryName]($this));
-		}
-		
+		$invokeMethod = $reflectionClass->getMethod($method);
+		$parameters = $invokeMethod->getParameters();
+		return $parameters;
 	}
-	
-	public function __call($funcName, $params)
-	{
-		echo '<pre/>';
-		echo $funcName;
-		var_dump($params);
-	}
-	
-	
 	
 	/**
 	 * Finds an entry of the container by its identifier and returns it.
@@ -74,15 +42,22 @@ class SContainer implements ContainerInterface
 	 */
 	public function get($id)
 	{
+		if(isset($this->_factories[$id]))
+		{
+			$this->invokeByFactory($id);
+		}
+		if(isset($this->_invokables[$id]))
+		{
+			$this->invokeByClass($id);
+		}
 		if(!$this->has($id))
 		{
-			#exit('Container does not have service with name '.$id);
 			throw new ServiceNotFoundException(sprintf('Container does not have service with name %s', $id));
 		}
 		$result = $this->_services[$id];
-		return $result->getService();
+		return $result;
 	}
-
+	
 	/**
 	 * Returns true if the container can return an entry for the given identifier.
 	 * Returns false otherwise.
@@ -93,16 +68,99 @@ class SContainer implements ContainerInterface
 	 */
 	public function has($id)
 	{
-		if(isset($this->_services[$id]))
-		{
-			return true;
-		}
-		return false;
+		return ( isset($this->_services[$id]) || isset($this->_factories[$id]) || isset($this->_invokables[$id]) );
 	}
 	
-
-	public function setService($serviceName, $service)
+	public function setInvokable($key, $value, $overwrite = true)
 	{
-		$this->_services[$serviceName] = $service;
+		if($overwrite == false && isset($this->_invokables[$key]))
+		{
+			throw new \Exception('Invokable already declared');
+		}
+		$this->_invokables[$key] = $value;
+	}
+
+	public function setFactory($key, $value, $overwrite = true)
+	{
+		if($overwrite == false && isset($this->_factories[$key]))
+		{
+			throw new \Exception('Factory already declared');
+		}
+		$this->_factories[$key] = $value;
+	}
+
+	public function invokeByFactory($id)
+	{
+		$factory = new $this->_factories[$id]();
+		$reflectionParams = $this->getMethodParams($factory, '__invoke');
+		$parameterList = array();
+		foreach($reflectionParams as $parameter)
+		{
+			$parameterClass = $parameter->getClass();
+			if($parameterClass != null)
+			{
+				$parameterList[$parameter->name] = $this->get($parameterClass->name);
+			}
+			else
+			{
+				$parameterList[$parameter->name] = $this->get($parameter->name);
+			}
+		}
+		$reflectionClass = new \ReflectionClass($factory);
+		$invokeMethod = $reflectionClass->getMethod('__invoke');
+		$invoked = $invokeMethod->invokeArgs($factory, $parameterList);
+		$this->setService($id, $invoked);
+		return true;
+	}
+
+	public function invokeByClass($id)
+	{
+		$invokable = $this->_invokables[$id];
+		$this->_services[$id] = new $invokable; 
+		return true;
+	}
+	
+	private function _setDependencies($configuration)
+	{
+		if(isset($configuration['dependencies']['factories']))
+		{
+			foreach($configuration['dependencies']['factories'] as $className => $factoryName)
+			{
+				$this->setFactory($className, $factoryName);
+			}
+		}
+		if(isset($configuration['dependencies']['invokables']))
+		{
+			foreach($configuration['dependencies']['invokables'] as $interface => $implementation)
+			{
+				$this->setInvokable($interface, $implementation);
+			}
+		}
+
+	}
+	
+	/**
+	 * constructor.
+	 * @access public
+	 * @param $config
+	 * @param SContainer|null $router
+	 */
+	public function __construct($configuration = array())
+	{
+		$this->setService('config', $configuration);
+		$this->setService(ContainerInterface::class, $this);
+		$this->setService(self::class, $this);
+		$this->setService('path', '/');
+		
+		$this->_setDependencies($configuration);
+		foreach($configuration as $config)
+		{
+			$this->_setDependencies($config);
+		}
+	}
+	
+	public function setService($key, $value)
+	{
+		$this->_services[$key] = $value;
 	}
 }
